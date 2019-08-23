630f8981d95d69ab8cdc855a885ff4009bc713b5
icesurf666 <icesurf@yandex.ru>
2019-08-23 18:45:51 +0700

    adaptive

--- js/common.js
+++ js/common.js
@@ -37,3 +37,10 @@ $(document).ready(function(){
     }
   });
 });
+
+$(document).ready(function() {
+  $('.menu-mobile, .menu-mobile__close').on('click', function(){
+    $('.menu-m').toggle();
+    return false;
+  })
+});
